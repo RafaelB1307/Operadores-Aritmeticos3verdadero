23d6c74f81a95e68ede9877f7f52eddad992dfde
RafaelB1307 <Rafael.bastides@uabc.edu.mx>
2022-05-21 00:57:38 -0700

    Agregar archivos de proyecto.

---
+++ Multiplicacion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplicacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 8;
+            int num2 = 9;
+
+            Console.WriteLine(num1 * num2);
+            Console.ReadKey();
+
+        }
+    }
+}
